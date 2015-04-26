81007924b676721a72dc0f08fd5b8a3fb59277e5
Nathan Levesque <rhysyngsun@gmail.com>
2015-04-25 20:06:24 -0400

    Added cli arg parsing via docopt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Usage: renderer <scene-file>
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_scene_file: String,
+}
+
 fn main() {
-    // TODO: get args/options
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
     // TODO: init
     // TODO: parse scene
     // TODO: cleanup
